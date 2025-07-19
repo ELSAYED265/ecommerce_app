4cbe6db81fb25970cd2a7cec77f830db0826b64c
ELSAYED265 <arrr9990@gmail.com>
2025-07-19 18:06:00 +0300

    frist commit

---
+++ lib/data/datasource/static/static.dart
@@ -0,0 +1,29 @@
+import 'package:ecommerce_app/core/constant/imageAsset.dart';
+import 'package:ecommerce_app/data/model/onboardingModel.dart';
+
+List<onBoardingModel> onBoardingList = [
+  onBoardingModel(
+    title: "Choose Product",
+    body:
+        "we have a 100k product choose /n your product from our e_/n commerce shope",
+    image: ImageAssets.onBoardingImageOne,
+  ),
+  onBoardingModel(
+    title: "Easy safe payment& ",
+    body:
+        "we have a 100k product choose /n your product from our e_/n commerce shope",
+    image: ImageAssets.onBoardingImageOne,
+  ),
+  onBoardingModel(
+    title: "trace your order",
+    body:
+        "we have a 100k product choose /n your product from our e_/n commerce shope",
+    image: ImageAssets.onBoardingImageOne,
+  ),
+  onBoardingModel(
+    title: "fast delivery",
+    body:
+        "we have a 100k product choose /n your product from our e_/n commerce shope",
+    image: ImageAssets.onBoardingImageOne,
+  ),
+];
